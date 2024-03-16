14d87d5db73961a62febcb2d9d734e55a1dca0a7
Königstein Nicole <nicole.koenigstein@students.fhnw.ch>
2024-03-16 08:01:02 +0100

    updated requirements

--- utils/requirements.py
+++ utils/requirements.py
@@ -2,8 +2,9 @@ import subprocess
 
 
 def install_packages():
-    packages = ["transformers == 4.26.1", "datasets == 2.10.1", "evaluate==0.4.0", "pyarrow==9.0.0",
-                "sentencepiece", "yfinance==0.2.37", "pandas_datareader==0.10.0", "pandas_market_calendars==4.4.0", "huggingface_hub==0.21.4"]
+    packages = ["transformers == 4.38.2", "evaluate==0.4.0", "pyarrow==9.0.0",
+                "sentencepiece", "yfinance==0.2.37", "pandas_datareader==0.10.0",
+                "pandas_market_calendars==4.4.0", "huggingface_hub==0.20.3"]
     check = u'\u2705'
     print("\033[1mInstalling requirements...\n\033[0m")
     for package in packages:
